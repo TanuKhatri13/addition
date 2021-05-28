b9bf2147518b87bc2e2fb6de7e7e743e3730b3e2
TanuKhatri13 <72642897+TanuKhatri13@users.noreply.github.com>
2021-05-29 00:06:51 +0530

    Update Addsub.java

--- Addsub.java
+++ Addsub.java
@@ -5,7 +5,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class add extends HttpServlet {
+public class addsub extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
